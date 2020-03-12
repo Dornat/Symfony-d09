7218165d27bb2714026ad1d3a95bb5f55bea6941
Dornat <mrpolosukhin@gmail.com>
2020-03-12 11:42:40 +0200

    Added migration and user fixtures.

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -2,15 +2,41 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixtures extends Fixture
 {
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $encoder;
+
+    /**
+     * UserFixtures constructor.
+     * @param UserPasswordEncoderInterface $encoder
+     */
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    /**
+     * @param ObjectManager $manager
+     */
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Faker\Factory::create();
+        for ($i = 0; $i < 42; $i++) {
+            $user = new User();
+            $user->setUsername($faker->userName);
+            $user->setPassword($this->encoder->encodePassword($user, '1234'));
+
+            $manager->persist($user);
+        }
 
         $manager->flush();
     }
